e080a62339f8cea9d4b980c2fd362699611c7dfc
Rehan Siraj <rsiraj@r2hstudent.org>
2022-01-24 16:22:07 -0500

    east 2 incomplete

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// EASY 1
 let array = [2, 4, 7, 11, 15, 16];
 const even = array.filter(evenPrint);
 const odd = array.filter(oddPrint);
@@ -12,3 +14,17 @@ function evenPrint(array){
 function oddPrint(array){
     return array % 2 != 0;
 }
+
+// EASY 2
+let numArray = [1,2,3,4,5,6,7,8,9,10];
+const prime = numArray.filter(primePrint);
+
+console.log(prime)
+
+function primePrint(numArray){
+    for (let i = 2; i < numArray; i++ ){
+        if (numArray % i != 0) {return numArray;}
+    }
+}
+
+// MEDIUM 1
