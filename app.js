fbbc631a99f4dc7a029776cffc90eb58f4799f20
Rehan Siraj <rsiraj@r2hstudent.org>
2022-01-26 10:32:58 -0500

    easy 2

--- app.js
+++ app.js
@@ -27,6 +27,19 @@ function primePrint(numArray){
     }
 }
 
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(num) {
+    for (let i = 2; num > i; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
+
+console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
+
 // MEDIUM 1
 let vowels = ["a", "e", "i", "o", "u"];
 
@@ -102,3 +115,5 @@ function gcd(num1, num2) {
 
 console.log(gcd(78, 126));
 console.log(gcd(336, 360));
+
+//
